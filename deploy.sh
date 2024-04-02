959983d2ce58d9e3a4b6a98e6bdbbb11e071dc6e
statik2002 <statik2002@mail.ru>
2024-04-02 13:51:49 +0800

    docker ++

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,16 @@ COMMIT_HASH=`git rev-parse HEAD`
 
 mkdir -p "frontend_deploy"
 
-cp -fr myshop/myshop_front/ frontend_deploy/
+cp -fr myshop/staticfiles staticfiles
+cp -fr myshop/myshop_front/dist staticfiles/
 
 docker compose build
 docker compose up -d
 
-docker compose stop web-static
-docker compose rm --force web-static
-docker compose stop parcel
-docker compose rm --force parcel
+#docker compose stop web-static
+#docker compose rm --force web-static
+#docker compose stop parcel
+#docker compose rm --force parcel
 
 docker compose exec -it web sh -c "python manage.py collectstatic --noinput"
 docker compose exec -it web sh -c "python manage.py migrate"
--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST'),
-        'PORT': env.int('DB_PORT'),
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST'),
+        'PORT': env.int('POSTGRES_PORT'),
     }
 }
 
